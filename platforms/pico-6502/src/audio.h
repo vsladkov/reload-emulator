131eb3d774256412007b5a9f6550194cac2dffa2
Veselin Sladkov <veselin.sladkov@gmail.com>
2024-06-26 15:45:36 +0300

    Fix problem with Olimex RP2040PC audio pin

--- platforms/pico-6502/src/audio.h
+++ platforms/pico-6502/src/audio.h
@@ -7,11 +7,11 @@
 extern "C" {
 #endif
 
-#ifdef OLIMEX_NEO6502
+#ifdef defined(OLIMEX_NEO6502) || defined(OLIMEX_RP2040PC)
 #define _AUDIO_PIN (20)
 #else
 #define _AUDIO_PIN (8)
-#endif  // OLIMEX_NEO6502
+#endif
 
 void audio_init(uint8_t audio_pin, uint16_t sample_freq);
 void audio_push_sample(const uint8_t sample);
